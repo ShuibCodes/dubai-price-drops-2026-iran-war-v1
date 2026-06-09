15b927268e22f6693b0f435b7ec7b0ab06e94438
Shuib Abdillahi <abdullahshuib124@gmail.com>
2026-06-09 09:44:58 +0400

    azim deal 24-units

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/live-updates/vip-deal-1",
+        destination: "/live-updates/vip-deal-1/index.html",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
